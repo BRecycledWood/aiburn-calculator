92df4abe0a86469b6227989387d0727501aba515
BRecycledWood <wood.brad@me.com>
2025-12-01 06:50:09 -0500

    fix: remove duplicate ErrorBoundary from main.jsx

--- aiburn-website/src/main.jsx
+++ aiburn-website/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ErrorBoundary } from './components/ErrorBoundary.jsx'
 import { initSentry } from './utils/sentry.js'
 import { initializeGA } from './utils/analytics.js'
 import './index.css'
@@ -16,8 +15,6 @@ if (typeof window !== 'undefined') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>,
 )
